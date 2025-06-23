ae2bfa0ba642b68b2841b10c13d7518df3b4e15a
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-06-23 17:09:54 +0200

    Feat: milestone 3 completato

--- 23-06-2025.sql
+++ 23-06-2025.sql
@@ -7,3 +7,8 @@ WHERE YEAR(`date_of_birth`) = 1990;
 SELECT *
 FROM `courses`
 WHERE `cfu` > 10;
+
+/* milestone 3 */
+SELECT *
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
